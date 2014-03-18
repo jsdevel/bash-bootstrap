9f73c2cf3faa5e3ea82260342b8ffb77a3c443cc
jsdevel <js.developer.undefined@gmail.com>
2014-03-18 00:51:26 -0700

    Adding setup for bash_aliases

--- bootstrap.bash
+++ bootstrap.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo 5;
+SCRIPT_DIR=$(dirname $(readlink -f ${BASH_SOURCE[@]}));
+SETUP_SCRIPTS=$SCRIPT_DIR/setup-scripts
+
+function setup(){
+  . $SETUP_SCRIPTS/$1
+}
+
+setup default-aliases
