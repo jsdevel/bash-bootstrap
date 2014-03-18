5da222892780cd26700323b0f76345b7b59e321a
jsdevel <js.developer.undefined@gmail.com>
2014-03-18 00:55:03 -0700

    consolidating date header

--- bootstrap.bash
+++ bootstrap.bash
@@ -3,6 +3,10 @@
 SCRIPT_DIR=$(dirname $(readlink -f ${BASH_SOURCE[@]}));
 SETUP_SCRIPTS=$SCRIPT_DIR/setup-scripts
 
+function addedHeader(){
+  echo "#Added by bash-bootstrap on \"`date`\""
+}
+
 function setup(){
   . $SETUP_SCRIPTS/$1
 }
